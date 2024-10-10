a82ccf13bf8c5a603423122f5ec27071f2b3f5b8
miguelcastroe <135630596+miguelcastroe@users.noreply.github.com>
2024-10-10 16:40:36 -0500

    Update drawbox.js

--- src/modules/drawbox.js
+++ src/modules/drawbox.js
@@ -1,5 +1,6 @@
 export function drawInfo(context, cursor, buffer) {
     context.fillStyle = 'white';
-    context.font = '12px monospace';
+    context.font = '16px monospace';
     context.fillText(`FPS: ${Math.round(1 / context.delta)}`, 10, 20);
 }
+
